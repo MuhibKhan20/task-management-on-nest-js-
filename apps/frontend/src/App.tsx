f60818254e4ec31854f8502897c4c0be7e5490ae
Muhib Khan <muhibhabibkhan2002@gmail.com>
2025-09-10 03:32:03 +0500

    🔧 Restore frontend - Accidentally deleted, now restored with updated .env for new backend

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import Box from '@mui/material/Box';
+import Routes from './Routes/Routes';
+import { ToastContainer } from 'react-toastify';
+import NotificationContainer from './components/Notifications/NotificationContainer';
+import { useDeadlineMonitor } from './hooks/useDeadlineMonitor';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  useDeadlineMonitor();
+
+  return (
+    <>
+      <Box display={'flex'} flexDirection={'column'} height={'100vh'}>
+        <Routes />
+      </Box>
+      <NotificationContainer />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
+
+export default App;
