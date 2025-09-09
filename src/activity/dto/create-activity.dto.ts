d89e5054eb43eb06010bf40f2eed584c996d6147
Muhib Khan <muhibhabibkhan2002@gmail.com>
2025-09-10 03:05:56 +0500

    🗑️ Remove duplicate src directory - Fix Render deployment conflicts

--- src/activity/dto/create-activity.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateActivityDto {
-  @IsString()
-  @IsNotEmpty()
-  title: string;
-}
--- src/auth/interfaces/auth.interface.ts
+++
@@ -1,3 +0,0 @@
-import { User } from '@prisma/client';
-
-export interface OmitUser extends Omit<User, 'password'> {}
